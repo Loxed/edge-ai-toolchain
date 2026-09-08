7dbe5c6b05f05e20b70861338607da05d7d09df1
Lox <leopold@rombaut.org>
2026-09-08 15:58:25 +0200

    chore: add tests for LogicGatesDataset and Perceptron model

--- tests/unit/test_and.py
+++ tests/unit/test_and.py
@@ -1,4 +1,5 @@
 from edgekit.datasets.logic_gates import LogicGatesDataset
+from edgekit.models.perceptron import Perceptron
 
 
 def test_and_dataset():
@@ -9,3 +10,13 @@ def test_and_dataset():
     assert dataset[1] == ([0, 1], 0)
     assert dataset[2] == ([1, 0], 0)
     assert dataset[3] == ([1, 1], 1)
+
+
+def test_perceptron_learns_and():
+    dataset = LogicGatesDataset("data/logic_gates/and.csv")
+    model = Perceptron(n_inputs=2)
+
+    model.fit(dataset, epochs=20)
+
+    for x, expected in dataset:
+        assert model.predict(x) == expected
